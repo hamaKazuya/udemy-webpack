b491156a0d11e5051a33a1bf807eeb28debd52b8
hamaKazuya <haman.kazuya@gmail.com>
2020-02-25 14:25:06 +0900

    implement url-loader and file-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import { Nijou, NAME } from './utilities'
 import './style.css'
+import logo from './logo.png'
 
 console.log(Nijou(2))
 
@@ -13,3 +14,7 @@ function component() {
 
 document.body.appendChild(component())
 document.body.classList.add('haikei')
+
+const image = new Image()
+image.src = logo
+document.body.appendChild(image)
